31e9880b6a8548cdf6e60ac6f331d46832d46f60
DUVAN0829 <duvancasgonz29@gmail.com>
2025-10-28 12:01:39 -0500

    Commit inicial

---
+++ src/test/java/co/duvan/security/app/DemoJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package co.duvan.security.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoJwtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
