3b1c19b2b75960372e15b857d8beaf78fb105d57
DUVAN0829 <duvancasgonz29@gmail.com>
2025-10-29 07:23:07 -0500

    modifica 'UserRepository' con método para buscar usuarios por el 'username'

--- src/main/java/co/duvan/security/app/repository/UserRepository.java
+++ src/main/java/co/duvan/security/app/repository/UserRepository.java
@@ -3,6 +3,10 @@ package co.duvan.security.app.repository;
 import co.duvan.security.app.model.User;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends CrudRepository<User, Long> {
+
+    Optional<User> findByUsername(String username);
 
 }
