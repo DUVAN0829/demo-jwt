1dfe4cb2587c1770ad34954df3a8853cf8c19ea2
DUVAN0829 <duvancasgonz29@gmail.com>
2025-10-29 06:36:33 -0500

    Añade clases de login en el directorio 'http' y crea filtro para obtener JWT

---
+++ src/main/java/co/duvan/security/app/http/AuthResponse.java
@@ -0,0 +1,22 @@
+package co.duvan.security.app.http;
+
+public class AuthResponse {
+
+    //* Vars
+    private String token;
+
+    //* Constructor
+    public AuthResponse(String token) {
+        this.token = token;
+    }
+
+    //* Getters and Setters
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+}
---
+++ src/main/java/co/duvan/security/app/http/RegisterRequest.java
@@ -0,0 +1,62 @@
+package co.duvan.security.app.http;
+
+public class RegisterRequest {
+
+    //* Vars
+    private String username;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private String country;
+
+    //* Constructor
+    public RegisterRequest(String username, String password, String firstName, String lastName, String country) {
+        this.username = username;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.country = country;
+    }
+
+    //* Getters and Setters
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+}
