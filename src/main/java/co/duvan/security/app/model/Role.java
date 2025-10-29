4934a6d986dc31dfda569db2600abd94612994e3
DUVAN0829 <duvancasgonz29@gmail.com>
2025-10-29 07:10:52 -0500

    Añade dos entidad 'User' y clase 'Role' para la persistencia en la base de datos y el manejo de roles

---
+++ src/main/java/co/duvan/security/app/model/Role.java
@@ -0,0 +1,11 @@
+package co.duvan.security.app.model;
+
+public enum Role {
+
+    ADMIN("Administrador"),
+    USER("Usuario");
+
+    Role(String value) {
+    }
+
+}
---
+++ src/main/java/co/duvan/security/app/model/User.java
@@ -0,0 +1,93 @@
+package co.duvan.security.app.model;
+
+import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Entity
+@Table(name = "users", uniqueConstraints = {@UniqueConstraint(columnNames = {"username"})})
+public class User implements UserDetails {
+
+    //* Vars
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String username;
+    private String lastName;
+    private String firstName;
+    private String country;
+    private String password;
+    private Role role;
+
+    //* Methods UserDetails
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public String getPassword() {
+        return "";
+    }
+
+    @Override
+    public String getUsername() {
+        return "";
+    }
+
+    //* Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+}
