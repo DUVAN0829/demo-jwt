9fdc42af610ec9d9bad6aa2cce2ec15d5ab875c6
DUVAN0829 <duvancasgonz29@gmail.com>
2025-10-29 07:12:30 -0500

    Crea repositorio para 'User'

---
+++ src/main/java/co/duvan/security/app/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package co.duvan.security.app.repository;
+
+import co.duvan.security.app.model.User;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<User, Long> {
+
+}
