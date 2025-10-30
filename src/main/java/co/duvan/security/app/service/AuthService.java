c90544c61685ec2c048ffd775801e10fdeb9169a
DUVAN0829 <duvancasgonz29@gmail.com>
2025-10-30 17:38:26 -0500

    Implemnta método login de 'AuthService'

--- src/main/java/co/duvan/security/app/service/AuthService.java
+++ src/main/java/co/duvan/security/app/service/AuthService.java
@@ -6,6 +6,9 @@ import co.duvan.security.app.http.RegisterRequest;
 import co.duvan.security.app.model.Role;
 import co.duvan.security.app.model.User;
 import co.duvan.security.app.repository.UserRepository;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -16,17 +19,25 @@ public class AuthService {
     private final UserRepository userRepository;
     private final JwtService jwtService;
     private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
 
     //* Constructor
-    public AuthService(UserRepository userRepository, JwtService jwtService, PasswordEncoder passwordEncoder) {
+    public AuthService(UserRepository userRepository, JwtService jwtService, PasswordEncoder passwordEncoder, AuthenticationManager authenticationManager) {
         this.userRepository = userRepository;
         this.jwtService = jwtService;
         this.passwordEncoder = passwordEncoder;
+        this.authenticationManager = authenticationManager;
     }
 
     //* Methods
     public AuthResponse login(LoginRequest request) {
-        return null;
+
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(request.getUsername(), request.getPassword()));
+        UserDetails user = userRepository.findByUsername(request.getUsername()).get();
+        String token = jwtService.getToken(user);
+
+        return new AuthResponse(token);
+
     }
 
     public AuthResponse register(RegisterRequest request) {
